bb80d4fec928689b2cf0430882c5c921d6f92927
Marcus Richard <msrichard18@gmail.com>
2016-01-28 11:04:06 -0500

    Added code to save tweet to database but getting an error

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,9 @@ class TweetsController < ApplicationController
 	end
 
 	def create
-
+		#Comment below is parameters from terminal.  They aree accessed in sublime by typing in 'params'
+		#  Parameters: {"utf8"=>"✓", "authenticity_token"=>"bm+GdCTOeYjEavpPYAHedymYT6IKCjBoYc9XstLiGhet4rhvciobGcwNP9ldM6EHYY6haG6ql4mr4sUIPdyV5A==", "tweet"=>{"content"=>"lets try to save"}, "commit"=>"Create Tweet"}
+		@tweet = Tweet.create(params[:tweet])
 		render text: 'hello'
 	end
 end
