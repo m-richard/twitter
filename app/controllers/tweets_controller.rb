5a509d1244a15359c1497ec35e6e0f8d85a89684
Marcus Richard <msrichard18@gmail.com>
2016-02-02 10:35:45 -0500

    did strong parameters, saved tweet to database

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,12 @@ class TweetsController < ApplicationController
 	def create
 		#Comment below is parameters from terminal.  They aree accessed in sublime by typing in 'params'
 		#  Parameters: {"utf8"=>"✓", "authenticity_token"=>"bm+GdCTOeYjEavpPYAHedymYT6IKCjBoYc9XstLiGhet4rhvciobGcwNP9ldM6EHYY6haG6ql4mr4sUIPdyV5A==", "tweet"=>{"content"=>"lets try to save"}, "commit"=>"Create Tweet"}
-		@tweet = Tweet.create(params[:tweet])
-		render text: 'hello'
+		@tweet = Tweet.create(tweet_params)
+		redirect_to new_tweet_path
+	end
+
+
+	def tweet_params
+		return params.require(:tweet).permit(:content)
 	end
 end
