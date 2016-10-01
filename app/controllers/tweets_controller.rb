b39441c92ce89306ac34af211ef50c8a700e84cc
Marcus Richard <msrichard18@gmail.com>
2016-10-01 12:41:21 -0400

    added index and show actions, added flash alert if tweet creation fails

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,26 +1,30 @@
 class TweetsController < ApplicationController
-	
+
 	before_action :authenticate_user!
+
+	def index
+		@tweets = Tweet.all
+	end
 
 	def new
 		@tweet = Tweet.new
 	end
 
+	def show
+		@tweet = Tweet.find(params[:id])
+	end
+
 	def create
-		#Comment below is parameters from terminal.  They aree accessed in sublime by typing in 'params'
-		#  Parameters: {"utf8"=>"✓", "authenticity_token"=>"bm+GdCTOeYjEavpPYAHedymYT6IKCjBoYc9XstLiGhet4rhvciobGcwNP9ldM6EHYY6haG6ql4mr4sUIPdyV5A==", "tweet"=>{"content"=>"lets try to save"}, "commit"=>"Create Tweet"}
 		@tweet = Tweet.new(tweet_params)
 		@tweet.user = current_user
 		if @tweet.save
-			flash[:success]='STFU'
+			flash[:success] = 'You successfully created a tweet!'
 			redirect_to new_tweet_path
 		else
-			# got rid of flash message because simple_Form includes errors
-			# flash[:danger] = "YOUR TWEET IS NOT GOOD ENOUGH"
-			render 'new' #new.html.erb, not def new
+			flash[:alert] = 'You failed.'
+			render 'new'
 		end
 	end
-
 
 	def tweet_params
 		return params.require(:tweet).permit(:content)
