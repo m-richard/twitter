cd369cbba9b5bfae74e7244675cd000abbbd078f
Marcus Richard <msrichard18@gmail.com>
2017-01-25 22:27:08 -0500

    changed length of tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,11 +1,11 @@
 class Tweet < ActiveRecord::Base
 	belongs_to :user
 
-	validates :content, length: {maximum: 5}
+	validates :content, length: {maximum: 300}
 
 	after_create :email_user
 
-	def email_user
-		TweetMailer.alert_user_about_tweet(self).deliver_now!
-	end
+	# def email_user
+	# 	TweetMailer.alert_user_about_tweet(self).deliver_now!
+	# end
 end
